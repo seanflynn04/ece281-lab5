311eef461058ed92909de00565fa8f217e923327
seanflynn04 <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2025-05-04 22:05:07 -0600

    update

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -24,12 +24,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library UNISIM;
+use UNISIM.VComponents.all;
 
 entity ALU is
     Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
@@ -41,7 +41,61 @@ end ALU;
 
 architecture Behavioral of ALU is
 
+    signal s_A : signed (7 downto 0);
+    signal s_B : signed (7 downto 0);
+    signal u_A : unsigned (7 downto 0);
+    signal u_B : unsigned (7 downto 0);
+    
+    signal C_out : std_logic;
+    signal result: std_logic_vector (7 downto 0);
+    signal a_result: std_logic_vector (8 downto 0);
+    signal s_result: std_logic_vector (8 downto 0);
+    
+    signal Z : std_logic_vector (3 downto 0);
+    signal N : std_logic_vector (3 downto 0);
+    signal C : std_logic_vector (3 downto 0);
+    signal V : std_logic_vector (3 downto 0);
+    
+   
 begin
 
+    s_A <= signed(i_A);
+    s_B <= signed(i_B);
+    u_A <= unsigned(i_A);
+    u_B <= unsigned(i_B);
 
+    result <=  std_logic_vector(s_A + s_B) when (i_op = "000") else
+    
+                 std_logic_vector(s_A - s_B) when (i_op = "001") else
+             
+                 (i_A and i_B) when (i_op = "010") else
+             
+                 (i_A or i_B) when (i_op = "011");
+                 
+    o_result <= result;
+                     
+    Z <= "0100" when (result = "00000000") else
+         "0000";
+    
+    N <= "1000" when (result(7) = '1') else
+         "0000";
+         
+    a_result <= std_logic_vector(u_A + u_B);  
+    s_result <= std_logic_vector(u_A - u_B);       
+    
+    C_out <= '1' when (a_result(8) = '1') or (s_result(8) = '1') else
+             '0';
+         
+    C <= "0010" when ((i_op = "000" or i_op = "001") and (C_out = '1')) else
+         "0000";    
+         
+    
+    
+    
+    
+    o_flags <= (Z + N);
+    
+    
+               
+              
 end Behavioral;
--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -39,7 +39,42 @@ end controller_fsm;
 
 architecture FSM of controller_fsm is
 
+type sm_state is (s_state0, s_state1, s_state2,s_state3);
+    
+    signal f_Q, f_Q_next: sm_state;
+
 begin
 
-
+    f_Q_next <= s_state0 when ((f_Q = s_state0)and(i_adv = '0')) or
+                              ((f_Q = s_state3)and(i_adv = '1')) else
+                              
+                s_state1 when ((f_Q = s_state1)and(i_adv = '0')) or
+                              ((f_Q = s_state0)and(i_adv = '1')) else  
+                              
+                s_state2 when ((f_Q = s_state2)and(i_adv = '0')) or
+                              ((f_Q = s_state1)and(i_adv = '1')) else 
+                               
+                s_state3 when ((f_Q = s_state3)and(i_adv = '0')) or
+                              ((f_Q = s_state2)and(i_adv = '1')) else  
+                              
+                              s_state0;   
+                              
+       with f_Q select
+        o_cycle <= "0001" when s_state0,
+                   "0010" when s_state1,
+                   "0100" when s_state2,
+                   "1000" when s_state3;
+                   
+                   
+       
+       register_proc : process (i_adv)
+begin
+     if (i_adv = '1' and i_reset = '0')then
+        f_Q <= f_Q_next;   
+     elsif (i_adv = '0' and i_reset = '1')then
+        f_Q <= s_state0;        
+     end if;    
+end process register_proc;     
+                          
+    
 end FSM;
